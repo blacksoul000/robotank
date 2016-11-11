0a06df24c7627d07bb1dd8fe217c775e460065fa
Vitaly Bondar <blacksoul000@gmail.com>
2016-11-11 19:18:40 +0000

    Calibration, minor fixes, renaming,

--- src/arduino_exchanger/src/arduino_exchanger/arduino_exchanger.cpp
+++ src/arduino_exchanger/src/arduino_exchanger/arduino_exchanger.cpp
@@ -3,6 +3,8 @@
 //msgs
 #include "robo_core/Influence.h"
 #include "robo_core/PointF.h"
+#include "robo_core/Point3D.h"
+#include <std_msgs/Empty.h>
 
 //ros
 #include <ros/ros.h>
@@ -14,16 +16,22 @@
 #include <termios.h>
 #include <unistd.h>
 #include <iostream>
+#include <chrono>
 
 using arduino::ArduinoExchanger;
+using Clock = std::chrono::high_resolution_clock;
 
 namespace
 {
 #pragma pack(push, 1)
     struct ArduinoPkg
     {
-        int16_t x = 0;
-        int16_t y = 0;
+        int16_t gunH = 0;
+        int16_t gunV = 0;
+        int16_t cameraV = 0;
+        int16_t yaw = 0;
+        int16_t pitch = 0;
+        int16_t roll = 0;
     };
     struct RaspberryPkg
     {
@@ -37,6 +45,7 @@ namespace
 
     constexpr double positionCoef = 360.0 / 32767;
     constexpr double influenceCoef = 90.0 / 32767;
+    const int timeout = 500; // ms
 } // namespace
 
 class ArduinoExchanger::Impl
@@ -47,25 +56,47 @@ public:
     std::vector< char > buffer;
     std::vector< char > prefix = { 0x55, 0x55 };
     bool waitPrefix = true;
+    Clock::time_point lastTime;
+    ArduinoPkg offsets;
+    ArduinoPkg lastData;
 
-    ros::Publisher positionPub;
+    std::list< ros::Subscriber > subscribers;
+
+    ros::Publisher gunPositionPub;
+    ros::Publisher cameraPositionPub;
+    ros::Publisher yprPub;
     ros::Subscriber influenceSub;
 
     bool open();
     bool isOpened() const;
     void readData();
+    void setArduinoOnline(bool set);
+
     void onInfluence(const robo_core::Influence& influence);
+    void onGunCalibrate(std_msgs::Empty);
+    void onCameraCalibrate(std_msgs::Empty);
+    void onGyroCalibrate(std_msgs::Empty);
 
     int setInterfaceAttribs (int fd, int speed, int parity);
     void setBlocking (int fd, int should_block);
+
+private:
+    bool arduinoOnline = true;
 };
 
 ArduinoExchanger::ArduinoExchanger(ros::NodeHandle* nh, const std::string& path):
     d(new Impl)
 {
     d->device = path;
-    d->positionPub = nh->advertise< robo_core::PointF >("camera/position", 1);
-    d->influenceSub = nh->subscribe("core/influence", 10, &ArduinoExchanger::Impl::onInfluence, d);
+    d->gunPositionPub = nh->advertise< robo_core::PointF >("gun/position", 1);
+    d->cameraPositionPub = nh->advertise< robo_core::PointF >("camera/position", 1);
+    d->yprPub = nh->advertise< robo_core::Point3D >("robo/ypr", 1);
+    d->subscribers.push_back(nh->subscribe("core/influence", 10, &ArduinoExchanger::Impl::onInfluence, d));
+    d->subscribers.push_back(nh->subscribe("gun/calibrate", 1, &ArduinoExchanger::Impl::onGunCalibrate, d));
+    d->subscribers.push_back(nh->subscribe("camera/calibrate", 1, &ArduinoExchanger::Impl::onCameraCalibrate, d));
+    d->subscribers.push_back(nh->subscribe("ypr/calibrate", 1, &ArduinoExchanger::Impl::onGyroCalibrate, d));
+
+    d->lastTime = Clock::now();
 }
 
 ArduinoExchanger::~ArduinoExchanger()
@@ -79,6 +110,10 @@ void ArduinoExchanger::process()
     if (!d->isOpened() && !d->open()) return;
 
     d->readData();
+    if (std::chrono::duration<double, std::milli>(Clock::now() - d->lastTime).count() > ::timeout) 
+    {
+        d->setArduinoOnline(false);
+    }
 }
 
 void ArduinoExchanger::Impl::readData()
@@ -93,9 +128,9 @@ void ArduinoExchanger::Impl::readData()
 
     if (!hasNewData) return;
 
-//    for (char cc: buffer)
-//        std::cout << std::hex << (unsigned short)cc << " ";
-//    std::cout << std::endl;
+    for (char cc: buffer)
+        std::cout << std::hex << (unsigned short)cc << " ";
+    std::cout << std::endl;
 
     if (waitPrefix)
     {
@@ -124,11 +159,30 @@ void ArduinoExchanger::Impl::readData()
         buffer.erase(buffer.begin(), buffer.begin() + packetSize);
         waitPrefix = true;
 
-        robo_core::PointF msg;
-        msg.x = pkg.x * ::positionCoef;
-        msg.y = pkg.y * ::positionCoef;
-        positionPub.publish(msg);
-        ROS_WARN("position: %d, %d, %f, %f", pkg.x, pkg.y, msg.x, msg.y);
+        robo_core::PointF gun;
+        gun.x = (pkg.gunH - offsets.gunH) * ::positionCoef;
+        gun.y = (pkg.gunV - offsets.gunV) * ::positionCoef;
+        gunPositionPub.publish(gun);
+
+        robo_core::PointF camera;
+        camera.x = 0;
+        camera.y = (pkg.cameraV - offsets.cameraV) * ::positionCoef;
+        cameraPositionPub.publish(camera);
+
+        robo_core::Point3D ypr;
+        ypr.x = (pkg.yaw - offsets.yaw) * ::positionCoef;
+        ypr.y = (pkg.pitch - offsets.pitch) * ::positionCoef;
+        ypr.z = (pkg.roll - offsets.roll) * ::positionCoef;
+        yprPub.publish(ypr);
+
+        memcpy(&lastData, &pkg, sizeof(ArduinoPkg));
+
+        ROS_WARN("gun: %f, %f, %d, %d", gun.x, gun.y, pkg.gunH, pkg.gunV);
+        ROS_WARN("camera: %f, %d", camera.y, pkg.cameraV);
+        ROS_WARN("ypr: %f, %f, %f, %d, %d, %d", ypr.x, ypr.y, ypr.z, pkg.yaw, pkg.pitch, pkg.roll);
+
+        lastTime = Clock::now();
+        setArduinoOnline(true);
     }
 }
 
@@ -225,6 +279,41 @@ void ArduinoExchanger::Impl::onInfluence(const robo_core::Influence& influence)
     write(fd, str.data(), str.length());
     if (write(fd, reinterpret_cast<const char *>(&pkg), sizeof(pkg)) != sizeof(pkg))
     {
-        ROS_WARN("Failed to write to the i2c bus.");
+        ROS_WARN("Failed to write to the bus.");
     }
+}
+
+void ArduinoExchanger::Impl::onGunCalibrate(std_msgs::Empty)
+{
+    ROS_WARN("Calibrate gun");
+    offsets.gunH = lastData.gunH;
+    offsets.gunV = lastData.gunV;
+}
+
+void ArduinoExchanger::Impl::onCameraCalibrate(std_msgs::Empty)
+{
+    ROS_WARN("Calibrate camera");
+    offsets.cameraV = lastData.cameraV;
+}
+
+void ArduinoExchanger::Impl::onGyroCalibrate(std_msgs::Empty)
+{
+    ROS_WARN("Calibrate gyro");
+    offsets.yaw = lastData.yaw;
+    offsets.pitch = lastData.pitch;
+    offsets.roll = lastData.roll;
+}
+
+void ArduinoExchanger::Impl::setArduinoOnline(bool online)
+{
+    if (online == arduinoOnline) return;
+    if (online)
+    {
+        ROS_WARN("Arduino online");
+    }
+    else
+    {
+        ROS_WARN("Arduino offline");
+    }
+    arduinoOnline = online;
 }
